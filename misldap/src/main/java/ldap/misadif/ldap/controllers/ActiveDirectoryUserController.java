481437e5df4ba908b888a8f2672547a46065c868
ZiyaTagizadeh <33990866+ZiyaTagizadeh@users.noreply.github.com>
2020-03-19 17:51:42 +0400

    Update ActiveDirectoryUserController.java

--- misldap/src/main/java/ldap/misadif/ldap/controllers/ActiveDirectoryUserController.java
+++ misldap/src/main/java/ldap/misadif/ldap/controllers/ActiveDirectoryUserController.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @RequestMapping("/ldap")
 public class ActiveDirectoryUserController {
     @Autowired
+
     @Qualifier("userActiveDirectoryService")
     private IUserService userService;
 
@@ -30,8 +31,7 @@ public class ActiveDirectoryUserController {
         System.out.println("Password="+passw);
         boolean isAuthenticated=false;
        ActiveDirectoryUser activeDirectoryUser = userService.findByLogin(login);
-        System.out.println("-------------------");
-        System.out.println("activeDirectoryUser"+activeDirectoryUser.getLdapLogin());
+
        if (Objects.nonNull(activeDirectoryUser)){
            //passwordu bura yaz ldap username pass deyirsen? beli
            ///sahir.azimli nin password un ok
